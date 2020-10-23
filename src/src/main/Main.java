c2c06fe5ee2fdc9f1e8f954209f797ec32b0639f
rsninja722 <jnick722547@gmail.com>
2020-10-23 00:43:28 -0400

    start

---
+++ src/src/main/Main.java
@@ -0,0 +1,73 @@
+package main;
+
+/**
+ * James N
+ * 2020.10.22
+ * The Last Raider
+ */
+
+import engine.*;
+
+public class Main extends GameJava {
+
+    enum State {
+        TITLE,
+        CUTSCENE,
+        TRANSITION,
+        PLAYING
+    }
+
+    State state = State.TITLE;
+
+    public static void main(String[] args) {
+        new Main();
+    }
+
+    public Main() {
+        super(800, 600, 60, 60);
+
+        LoopManager.startLoops(this);
+    }
+
+    @Override
+    public void update() {
+        switch(state) {
+            case TITLE:
+                break;
+            case CUTSCENE:
+                break;
+            case TRANSITION:
+                break;
+            case PLAYING:
+                break;
+        }
+    }
+
+    @Override
+    public void draw() {
+        switch(state) {
+            case TITLE:
+                break;
+            case CUTSCENE:
+                break;
+            case TRANSITION:
+                break;
+            case PLAYING:
+                break;
+        }
+    }
+
+    @Override
+    public void absoluteDraw() {
+        switch(state) {
+            case TITLE:
+                break;
+            case CUTSCENE:
+                break;
+            case TRANSITION:
+                break;
+            case PLAYING:
+                break;
+        }
+    }
+}
